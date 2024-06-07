03e802b8b874013ca186b7c05aeafc4bf66705f6
Shekhar <mauryashekhar9837@gmail.com>
2024-06-07 22:07:14 +0530

    .

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -21,21 +21,21 @@ const SearchResults = ({ PageUp }) => {
   }, [query]);
 
   return (
-    <div className=" relative flex flex-col items-center justify-start">
-      <div className="md:w-[70%] w-full flex flex-col items-start justify-center  mt-10 md:px-4 px-4">
-
+    <div className="relative flex flex-col items-center justify-start">
+      <div className="md:w-[70%] w-full flex flex-col items-start justify-center mt-10 md:px-4 px-4">
         <h2 className="py-4 text-black px-2">Search Results for: {query}</h2>
-        <div className=" px-2  grid md:grid-cols-4 grid-cols-2 grid-row-auto text-white md:gap-2 gap-0">
+        <div className="px-2 grid md:grid-cols-4 grid-cols-2 grid-row-auto text-white md:gap-2 gap-0">
           {results.length > 0 ? (
             results.map((item) => (
-              <Link to={`/products/${item.handle}`} onClick={() => PageUp()}>
-              <div
+              <Link
+                to={`/products/${item.handle}`}
+                onClick={PageUp}
                 key={item.id}
-                className="relative cursor-pointer md:p-0 p-1"
               >
+                <div className="relative cursor-pointer md:p-0 p-1">
                   <div className="text-white text-[11px] absolute inset-0 md:p-6 p-4 md:opacity-0 hover:opacity-100 transition-opacity">
                     <h1 className="md:flex hidden">{item.vendor}</h1>
-                    <h1 className="mt-4 text-sm ">{item.title}</h1>
+                    <h1 className="mt-4 text-sm">{item.title}</h1>
                     <p className="mt-2 text-[11px]">${item.price / 100}.00</p>
                   </div>
                   <img
@@ -44,8 +44,8 @@ const SearchResults = ({ PageUp }) => {
                     src={item.images}
                     alt={item.title}
                   />
-              </div>
-                </Link>
+                </div>
+              </Link>
             ))
           ) : (
             <p className="text-black">No results found</p>
